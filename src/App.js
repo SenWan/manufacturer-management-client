a0c6c95338ba8719582708dfd544797e5bd254c2
U Sen Wan <senwan96@gmail.com>
2022-05-26 22:07:31 +0600

    purchase page added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './Pages/Home/Home/Home';
 import Login from './Pages/Login/Login/Login';
 import Register from './Pages/Login/Login/Register/Register';
 import PortFolio from './Pages/PortFolio/PortFolio';
+import Purchase from './Pages/Purchase/Purchase';
 import Footer from './Pages/Shared/Footer/Footer';
 import Header from './Pages/Shared/Header/Header';
 import NotFound from './Pages/Shared/NotFound/NotFound';
@@ -18,6 +19,7 @@ function App() {
         <Route path='/home' element={<Home></Home>}></Route>
         <Route path='/blogs' element={<Blogs></Blogs>}></Route>
         <Route path='/portfolio' element={<PortFolio></PortFolio>}></Route>
+        <Route path='/purchase/:purchaseId' element={<Purchase></Purchase>}></Route>
         <Route path='/login' element={<Login></Login>}></Route>
         <Route path='/register' element={<Register></Register>}></Route>
         <Route path='*' element={<NotFound></NotFound>}></Route>
--- src/Pages/Home/Product/Product.js
+++ src/Pages/Home/Product/Product.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import './Product.css'
 
 const Product = ({product}) => {
-    const {name, img, description, quantity, price, minimum_order, available_quantity} = product;
+    const {name, _id, img, description, quantity, price, minimum_order, available_quantity} = product;
+
+    const navigate = useNavigate();
+    const navigatePurchase = (_id) => {
+        navigate(`/purchase/${_id}`)
+    }
     return (
         <div className='tools'>
             <img className='w-100 image' src={img} alt="" />
@@ -13,7 +19,7 @@ const Product = ({product}) => {
                 <h6>Minimum Order: {minimum_order}</h6>
                 <h6>Available Quantity: {available_quantity}</h6>
                 <p>{description}</p>
-                <button className='btn btn-primary w-100'>Book Now</button>
+                <button onClick={()=> navigatePurchase(`ID: ${_id}` )} className='btn btn-primary w-100'>Buy Now</button>
             </div>
         </div>
     );
